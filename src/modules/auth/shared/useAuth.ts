1ec8a3b8067b4e1cd8e41f933c1cb60d07a70123
DonFredi <dondadi.fn@gmail.com>
2026-06-22 15:14:47 +0300

    Fixed the no user bug

--- src/modules/auth/shared/useAuth.ts
+++ src/modules/auth/shared/useAuth.ts
@@ -1,10 +1,11 @@
 "use client";
 import { useAuthContext } from "@/providers/AuthProvider";
+import { redirect, useRouter } from "next/navigation";
 
 export const useAuth = () => {
   const { user, profile, isAuthenticated, isInitialized } = useAuthContext();
-  if (!user || !profile) {
-    throw new Error("useAuthUser must be used inside protected routes");
+  if (isInitialized && !user) {
+    redirect("/auth/login");
   }
   return { user, profile, isAuthenticated, isInitialized };
 };
